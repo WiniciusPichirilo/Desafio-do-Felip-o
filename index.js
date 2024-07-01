ece6904ebc166504dbdde3dae411553911c8011d
Winicius Pichirilo <winy162010@hotmail.com>
2024-07-01 19:38:56 -0300

    Desafio Flipão

---
+++ index.js
@@ -0,0 +1,29 @@
+function calcularSaldoEVictorias(vitorias, derrotas) {
+    const saldoVitorias = vitorias - derrotas;
+    let nivel;
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 11 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else if (vitorias >= 101) {
+        nivel = "Imortal";
+    }
+
+    return `O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivel}`;
+}
+
+// Exemplo de uso:
+const vitorias = parseInt(gets()); // Recebe a quantidade de vitórias
+const derrotas = parseInt(gets()); // Recebe a quantidade de derrotas
+
+const resultado = calcularSaldoEVictorias(vitorias, derrotas);
+print(resultado);
